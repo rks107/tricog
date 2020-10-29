ec34faee3f71e7417d8c88d0e57ebf2e587aef02
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-10-29 11:46:35 +0530

    create mysql database online

--- config/mysql.js
+++ config/mysql.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 
 var config = {
-  host: "127.0.0.1",
-  user: "root",
-  password: "Rohit@107",
-  database: "tricog",
+  host: "sql12.freemysqlhosting.net",
+  user: "sql12373338",
+  password: "VxnL7H3adh",
+  database: "sql12373338",
   port: 3306,
 };
+// var config = {
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "Rohit@107",
+//   database: "tricog",
+//   port: 3306,
+// };
 
 const conn = new mysql.createConnection(config);
 
